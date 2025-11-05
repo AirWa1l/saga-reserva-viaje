ac3dadbfa2507baf68c12e41726c9f8c4602e03a
Jose Adrian Marin <jose.adrian.marin@correounivalle.edu.co>
2025-11-04 20:44:11 -0500

    Added lyrics_service by JoseMarin

--- constructor.sh
+++ constructor.sh
@@ -9,3 +9,13 @@ docker build -t car-service:latest ./car_service
 
 # Exponer servicio para pruebas con Postman
 # kubectl port-forward svc/lyrics-service 5001:5001
+# kubectl port-forward svc/composition-service 5002:5002
+
+# kubectl delete -f .\k8s.yaml
+# kubectl get all
+
+# Eliminar imagenes construidas
+# docker rmi lyrics-service:latest
+# docker rmi hotel-service:latest
+# docker rmi car-service:latest
+# docker rmi orchestrator:latest
